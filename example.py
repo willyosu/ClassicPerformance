8cddf1476ded8ebd36dbcb1d67433f9b506d8f95
Willy <11691902+willyosu@users.noreply.github.com>
2019-06-01 00:03:33 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,16 +1,21 @@
-import ClassicPerformance as cpp
+import ActivePerformance as app
 
 # Calculating a single play
 # args: Rank, Accuracy, Mods, Age, Difficulty, Popularity
-singlePP = cpp.calcPlay(1, 99.83, 1.075, 983, 7.29, 209348)
+singlePP = app.calcPlay(1, 983, 896)
 
 # Getting performace from the user 'Cookiezi' via webscrape
-scrapePP = cpp.scrapePerformance("Cookiezi")
+scrapePP = app.scrapePerformance("Cookiezi")
 
 # Calculating total from score array of 2 scores
 userScores = [[1, 99.83, 1.075, 983, 7.29, 209348],[3, 97.33, 1.125, 381, 5.65, 36859]]
-calcPP = cpp.classicPerformance(userScores)
+calcPP = app.activePerformance(userScores)
 
 print("Single play pp: " + str(singlePP))
 print("Scraped pp: " + str(scrapePP))
 print("Calculated pp: " + str(calcPP))
+
+# File print the performance total for the user 'Cookiezi' via webscrape
+ap = ("ap:" + str("{:,}".format(round(app.scrapePerformance("Cookiezi")))))
+with open('ap.txt', 'w') as f:
+  print(ap, file=f)
