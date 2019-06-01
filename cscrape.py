8053827714835f80ad5d8a772bd667a403fa795f
Willy <11691902+willyosu@users.noreply.github.com>
2019-06-01 19:45:20 -0400

    Fixed WebDriverWait TimeOutException and optimized webscrape function

--- cscrape.py
+++ cscrape.py
@@ -1,5 +1,4 @@
 import datetime
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,37 +9,35 @@ from webdriver_manager.chrome import ChromeDriverManager
 # Unreliable for now, but it's the best set we have we have to estimate a user's performance.
 def get(Player, Age_Falloff):
 	# Takes date and return the number of days since (Y-M-D format)
-	def Since(Date_Play):
-		Date_Play = datetime.datetime.strptime(Date_Play, "%Y-%m-%d")
-		Date_Current = datetime.datetime.today()
-		delta = Date_Current - Date_Play
+	def Since(dateOfPlay):
+		dateOfPlay = datetime.datetime.strptime(dateOfPlay, "%Y-%m-%d")
+		dateOfToday = datetime.datetime.today()
+		delta = dateOfToday - dateOfPlay
 		return delta.days
 	
 	# Creating arrays for storing the data
-	Links = []
-	Ranks = []
-	Hits = []
-	Misses = []
-	Accuracies = []
-	ModCombo = []
-	Dates = []
-	Difficulties = []
-	Popularities = []
+	arrayOfLinks = []
+	arrayOfRanks = []
+	arrayOfHits = []
+	arrayOfMisses = []
+	arrayOfAccs = []
+	arrayOfModCombos = []
+	arrayOfDates = []
+	arrayOfStars = []
+	arrayOfPlaycount = []
 	endOfResults = False
 	
 	driver = webdriver.Chrome(ChromeDriverManager().install())
 	driver.get("https://osustats.ppy.sh/u/" + Player + "//1//2////1-500/")
-	element_present = expected_conditions.presence_of_element_located((By.XPATH, """/html/body/div/div/div/div[2]/div[1]/div/div[1]"""))
-	WebDriverWait(driver, 5).until(element_present)
+	WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, """//*[@id="line-chart"]""")))
 	Pages = driver.find_element_by_xpath("""/html/body/div/div/div/div[2]/div[1]/div/div[1]""").get_attribute("textContent")
-	Pages.split(" of ")
-	Pages = Pages[-2] + Pages[-1]
+	Pages = Pages.replace(" ","").split("of")
+	numOfPages = Pages[-1]
 	
 	# Creating webdriver to fetch data
 	def Page(Page_Number):
 		driver.get("https://osustats.ppy.sh/u/" + Player + "//" + Page_Number + "//2////1-500/")
-		element_present = expected_conditions.presence_of_element_located((By.XPATH, """/html/body/div/div/div/div[2]/div[1]/div/div[1]"""))
-		WebDriverWait(driver, 5).until(element_present)
+		WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, """//*[@id="line-chart"]""")))
 		
 		Beatmap_Data = driver.find_elements_by_css_selector("div.beatmapInfo > div > a")
 		Rank_Data = driver.find_elements_by_class_name("rank")
@@ -54,7 +51,7 @@ def get(Player, Age_Falloff):
 		# Getting links to each beatmap page
 		for i in Beatmap_Data:
 			i = i.get_attribute("href")
-			Links.append(i)
+			arrayOfLinks.append(i)
 		
 		# Converting rank data to numbers and adding to array
 		for i in Rank_Data:
@@ -67,21 +64,21 @@ def get(Player, Age_Falloff):
 			except:
 				pass
 			else:
-				Ranks.append(i)
+				arrayOfRanks.append(i)
 		
 		# Adding hit data to array
 		for i in Hit_Data:
-			Hits.append(i.text)
+			arrayOfHits.append(i.text)
 		
 		# Adding miss data to array
 		for i in Miss_Data:
-			Misses.append(i.text)
+			arrayOfMisses.append(i.text)
 		
 		# Zipping hit and miss data into an array of tuples
-		AccuracyData = zip(Hits, Misses)
+		Accuracy_Data = zip(arrayOfHits, arrayOfMisses)
 		
 		# Converting hit and miss data and calculating accuracy
-		for i in AccuracyData:
+		for i in Accuracy_Data:
 			hitList = i[0].split("/")
 			hitList.append(i[1])
 			try:
@@ -90,7 +87,7 @@ def get(Player, Age_Falloff):
 				pass
 			else:
 				hitList = list(map(int, hitList))
-				Accuracies.append((((hitList[0])+(hitList[1]*(1/3))+(hitList[2]*(1/6)))/(sum(hitList)))*100)
+				arrayOfAccs.append((((hitList[0])+(hitList[1]*(1/3))+(hitList[2]*(1/6)))/(sum(hitList)))*100)
 		
 		# Mods
 		for i in ModCombo_Data:
@@ -112,7 +109,7 @@ def get(Player, Age_Falloff):
 			except:
 				pass
 			else:
-				ModCombo.append(modSum)
+				arrayOfModCombos.append(modSum)
 		
 		# Checking date data and adding to array
 		for i in Date_Data:
@@ -126,25 +123,25 @@ def get(Player, Age_Falloff):
 					endOfResults = True
 					pass
 				else:
-					Dates.append(Since(i))
+					arrayOfDates.append(Since(i))
 		
 		# Fetching difficulty and popularity data from each beatmap
-		for i in Links:
+		for i in arrayOfLinks:
 			driver.get(i)
 			Difficulty_Data = driver.find_element_by_css_selector("table.beatmap-stats-table > tbody > tr:last-child > td.beatmap-stats-table__value")
 			Popularity_Data = driver.find_element_by_class_name("beatmap-success-rate__percentage")
 			Popularity_Data = Popularity_Data.get_property("title")
 			Popularity_Data = Popularity_Data.split(" / ")
 			PopularityString = Popularity_Data[0].replace(",","")
-			Difficulties.append(float(Difficulty_Data.text))
-			Popularities.append(int(PopularityString))
+			arrayOfStars.append(float(Difficulty_Data.text))
+			arrayOfPlaycount.append(int(PopularityString))
 		
-	for i in range(1, int(Pages) + 1):
+	for i in range(1, int(numOfPages) + 1):
 		Page(str(i))
-		if (endOfResults) or (i == int(Pages)):
+		if (endOfResults) or (i == int(numOfPages)):
 			Scores = []
-			for j in range(len(Dates)):
-				Scores.append(tuple((Ranks[j], Accuracies[j], ModCombo[j], Dates[j], Difficulties[j], Popularities[j])))
+			for j in range(len(arrayOfDates)):
+				Scores.append(tuple((arrayOfRanks[j], arrayOfAccs[j], arrayOfModCombos[j], arrayOfDates[j], arrayOfStars[j], arrayOfPlaycount[j])))
 			return(Scores)
 		else:
 			pass
