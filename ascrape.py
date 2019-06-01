83e36d002c0e943ca968e54cdd47560884c4f7d2
Willy <11691902+willyosu@users.noreply.github.com>
2019-06-01 19:45:45 -0400

    Fixed WebDriverWait TimeOutException and optimized webscrape function

--- ascrape.py
+++ ascrape.py
@@ -1,5 +1,4 @@
 import datetime
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,31 +9,29 @@ from webdriver_manager.chrome import ChromeDriverManager
 # Unreliable for now, but it's the best set we have we have to estimate a user's performance.
 def get(Player):
 	# Takes date and return the number of days since (Y-M-D format)
-	def Since(Date_Play):
-		Date_Play = datetime.datetime.strptime(Date_Play, "%Y-%m-%d")
-		Date_Current = datetime.datetime.today()
-		delta = Date_Current - Date_Play
+	def Since(dateOfPlay):
+		dateOfPlay = datetime.datetime.strptime(dateOfPlay, "%Y-%m-%d")
+		dateOfToday = datetime.datetime.today()
+		delta = dateOfToday - dateOfPlay
 		return delta.days
 	
 	# Creating arrays for storing the data
-	Ranks = []
-	Dates = []
-	Performances = []
+	arrayOfRanks = []
+	arrayOfDates = []
+	arrayOfPerformances = []
 	endOfResults = False
 	
 	driver = webdriver.Chrome(ChromeDriverManager().install())
 	driver.get("https://osustats.ppy.sh/u/" + Player + "//1//2////1-500/")
-	element_present = expected_conditions.presence_of_element_located((By.XPATH, """/html/body/div/div/div/div[2]/div[1]/div/div[1]"""))
-	WebDriverWait(driver, 5).until(element_present)
+	WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, """//*[@id="line-chart"]""")))
 	Pages = driver.find_element_by_xpath("""/html/body/div/div/div/div[2]/div[1]/div/div[1]""").get_attribute("textContent")
-	Pages.split(" of ")
-	Pages = Pages[-2] + Pages[-1]
+	Pages = Pages.replace(" ","").split("of")
+	numOfPages = Pages[-1]
 	
 	# Creating webdriver to fetch data
 	def Page(Page_Number):
 		driver.get("https://osustats.ppy.sh/u/" + Player + "//" + Page_Number + "//2////1-500/")
-		element_present = expected_conditions.presence_of_element_located((By.XPATH, """/html/body/div/div/div/div[2]/div[1]/div/div[1]"""))
-		WebDriverWait(driver, 5).until(element_present)
+		WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, """//*[@id="line-chart"]""")))
 		
 		Rank_Data = driver.find_elements_by_class_name("rank")
 		Date_Data = driver.find_elements_by_class_name("date")
@@ -52,7 +49,8 @@ def get(Player):
 			except:
 				pass
 			else:
-				Ranks.append(i)
+				arrayOfRanks.append(i)
+		
 		# Checking date data and adding to array
 		for i in Date_Data:
 			i = i.text
@@ -61,8 +59,9 @@ def get(Player):
 			except:
 				pass
 			else:
-				Dates.append(Since(i))
-		# Adding pp data to array
+				arrayOfDates.append(Since(i))
+		
+		# Fetching difficulty and popularity data from each beatmap
 		for i in Performance_Data:
 			i = i.text
 			remove = "-"
@@ -73,13 +72,14 @@ def get(Player):
 			except:
 				pass
 			else:
-				Performances.append(i)
-	for i in range(1, int(Pages) + 1):
+				arrayOfPerformances.append(i)
+		
+	for i in range(1, int(numOfPages) + 1):
 		Page(str(i))
-		if (endOfResults) or (i == int(Pages)):
+		if (endOfResults) or (i == int(numOfPages)):
 			Scores = []
-			for j in range(len(Dates)):
-				Scores.append(tuple((Ranks[j], Dates[j], Performances[j])))
+			for j in range(len(arrayOfDates)):
+				Scores.append(tuple((arrayOfRanks[j], arrayOfDates[j], arrayOfPerformances[j])))
 			return(Scores)
 		else:
 			pass
